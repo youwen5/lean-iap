81edfe41243a6d0fd30af48cd9a14c4b7f17a1cf
Anthony Wang <a@unnamed.website>
2026-01-04 21:51:06 -0600

    Finish first two psets

--- Pset1.lean
+++ Pset1.lean
@@ -3,21 +3,7 @@ import Mathlib
 /-
 # Pset 1
 
-## Problem 1.1
-
-Welcome to the first pset! Follow these instructions for installing VS Code and Lean: https://lean-lang.org/install/
-
-Next, clone the Git repo for this class and open it in VS Code: https://forgejo.mit.edu/xy/lean-iap
-
-You might have to wait a few minutes for Lean to download Mathlib. Once the yellow loading bars on the left go away, put your cursor on the `#eval` line. If it says 6 in the InfoView on the right, you're ready to Lean!
--/
-
-open Nat
-
-#eval 3!
-
-/-
-## Problem 1.2
+## 1.1
 
 Visit https://adam.math.hhu.de/#/ and try one of the games! I recommend:
 
@@ -25,7 +11,19 @@ Visit https://adam.math.hhu.de/#/ and try one of the games! I recommend:
 
 - Set Theory Game: Another great intro to Lean with a focus on set theory
 
-- Linear Algebra Game: TODO desc
+- Linear Algebra Game: I haven't played much of this one but it seems pretty good
 
-- Real Analysis, The Game: Some of the prose is currently AI slop but the devs said they're working on rewriting all the slop
+- Real Analysis, The Game: Great except that some of the prose is currently AI slop, although the devs said they're going to rewrite all the slop
+
+## 1.2
+
+Follow these instructions for installing VS Code and Lean on your computer: https://lean-lang.org/install/
+
+Next, clone the Git repo for this class and open the repo in VS Code: https://forgejo.mit.edu/xy/lean-iap
+
+You might have to wait a few minutes for Lean to download Mathlib. Now open this file, and once the yellow loading bars on the left go away, put your cursor on the `#eval` line. If it says 6 in the InfoView on the right, then you're ready to Lean!
 -/
+
+open Nat
+
+#eval 3!
--- Pset2.lean
+++ Pset2.lean
@@ -3,14 +3,37 @@ import Mathlib
 /-
 # Pset 2
 
-## Problem 2.1
+## 2.1
 
-Prove the following lemmas:
+Prove the following lemmas in Lean.
 -/
 
-example : 2 + 2 = 4 := by
+lemma two_plus_two : 2 + 2 = 4 := by
   sorry
 
+lemma and_swap : p ∧ q → q ∧ p := by
+  sorry
+
+lemma add_rearrange {a b c d : ℝ} : 0 ≤ a + b + c + d ↔ -b - c ≤ a + d := by
+  sorry
+
+lemma three_not_even {x : ℤ} : 2 * x ≠ 3 := by
+  sorry
+
+lemma linear_arithmetic {a b c d e f : ℝ} :
+    2 * a + b ≥ 1 →
+    b ≥ 0 → c ≥ 0 → d ≥ 0 → e * f ≥ 0 →
+    a ≥ 3 * c →
+    c ≥ 6 * e * f → d - f * e * 5 ≥ 0 →
+    a + b + 3 * c + d + 2 * e * f < 0 →
+    False := by
+  sorry
+
+lemma cross_multiply_field [Field α] {x y z w : α} :
+    x / y = z / w →
+    y ≠ 0 → w ≠ 0 →
+    x * w = z * y := by
+  sorry
 
 abbrev SolutionSet (y : ℝ) : Set (ℝ × ℝ × ℝ × ℝ × ℝ) :=
  {(x₁, x₂, x₃, x₄, x₅) |
@@ -23,8 +46,7 @@ abbrev SolutionSet (y : ℝ) : Set (ℝ × ℝ × ℝ × ℝ × ℝ) :=
     x₄ = -(y * t) - y * s ∧
     x₅ = y * s - t)}
 
-/-- Problem 4 from IMO 1963 -/
-example : (x₁, x₂, x₃, x₄, x₅) ∈ SolutionSet y ↔
+lemma imo1963_p4 : (x₁, x₂, x₃, x₄, x₅) ∈ SolutionSet y ↔
     (x₅ + x₂ = y * x₁ ∧
      x₁ + x₃ = y * x₂ ∧
      x₂ + x₄ = y * x₃ ∧
@@ -32,44 +54,70 @@ example : (x₁, x₂, x₃, x₄, x₅) ∈ SolutionSet y ↔
      x₄ + x₁ = y * x₅) := by
   sorry
 
-
 /-
-## Problem 2.2
+## 2.2
 
-Prove the following lemmas:
+Prove the following lemmas.
 -/
 
-example : n ≤ 2 ^ n := by
+lemma pow_two_larger : n ≤ 2 ^ n := by
   sorry
 
+lemma pow_two_larger' (h : 2 ≤ n) : (n - 2) * 2 ≤ 2 ^ (n - 1) := by
+  sorry
 
 /-
-## Problem 2.3
+## 2.3
 
-lsb
+Prove these lemmas about binary operations (taken from https://cjquines.com/files/binaryoperations.pdf).
 -/
+
+lemma cjq1 (f : α → α → α) (hl : ∀ x, f l x = x) (hr : ∀ x, f x r = x) : l = r := by
+  sorry
+
+lemma cjq2 [Nonempty α] (f : α → α → α) (h : ∀ x y, ∃ z, f x z = y ∧ ∀ z', f x z = f x z' → z = z')
+    : ∃ g : α → α → α, ∀ x y, f x (g x y) = y ∧ g x (f x y) = y := by
+  sorry
+
+lemma cjq3 (f g : α → α → α) (hid : ∀ x, f i x = x ∧ f x i = x ∧ g j x = x ∧ g x j = x)
+    (h : ∀ x y z w, f (g x y) (g z w) = g (f x z) (f y w)) : f = g := by
+  sorry
 
 /-
-## Problem 2.4
+## 2.4
 
-PPM parser
-
+Write a recursive function that computes the index of least significant bit of a natural number `x`, i.e. the largest `k` such that `2 ^ k` divides `x`. Prove your that your function terminates and is correct.
 -/
+
+def lsb (x : ℕ) (hx : 0 < x) : ℕ :=
+  sorry
+
+lemma lsb_div (x : ℕ) (hx : 0 < x) : 2 ^ lsb x hx ∣ x := by
+  sorry
+
+lemma lsb_largest (x : ℕ) (hx : 0 < x) : ∀ k > lsb x hx, ¬2 ^ k ∣ x := by
+  sorry
 
 /-
-## Problem 2.5
+## 2.5
 
-Quine
-
+Create a `structure` called `Color` to represent RGB colors. Then write a function that parses a [P6 PPM image](https://en.wikipedia.org/wiki/Netpbm#PPM_example) and returns an `Except` type with either an `Array (Array Color)` or an error message string. Prove that all array accesses are in bounds.
 -/
+
+def parsePPM (bytes : ByteArray) := do
+  sorry
 
 /-
-## Problem 2.6
+## 2.6
 
+Create a new file called `Quine.lean` and add it as a `[[lean_exe]]` in the `lakefile.toml` file. Then write a [quine](https://en.wikipedia.org/wiki/Quine_(computing)) in Lean and run it with `lake exe`.
 
+## 2.7
+
+Prove the following lemma.
 -/
 
-example [DecidableEq β] {A : Finset α} {g : α → Finset β}
+lemma crazy_lemma [DecidableEq β] {A : Finset α} {g : α → Finset β}
     (hin : ∀ y, x ∈ g y → g (f y) ⊂ g y)
     (hnin : ∀ y, x ∉ g y → g (f y) = g y)
     (hf : ∀ y, x ∉ g (f y))
@@ -78,18 +126,47 @@ example [DecidableEq β] {A : Finset α} {g : α → Finset β}
   sorry
 
 /-
-## Problem 2.7
+## 2.8
 
-https://cjquines.com/files/binaryoperations.pdf
+Implement applicative `seq` for a monad, first without using `do` notation, and then simplify your implementation using `do`.
+
+Furthermore, given the `fish` operator, implement the other two equivalent formulations of monads.
 -/
 
-example (f : α → α → α) l r (hl : ∀ x, f l x = x) (hr : ∀ x, f x r = x) : l = r := by
+def appSeq [Monad m] (fs : m (α → β)) (as : m α) : m β :=
   sorry
 
-example [Nonempty α] (f : α → α → α) (h : ∀ x y, ∃ z, f x z = y ∧ ∀ z', f x z = f x z' → z = z')
-    : ∃ g : α → α → α, ∀ x y, f x (g x y) = y ∧ g x (f x y) = y := by
+def appSeqDo [Monad m] (fs : m (α → β)) (as : m α) : m β := do
   sorry
 
-example (f g : α → α → α) i j (hid : ∀ x, f i x = x ∧ f x i = x ∧ g j x = x ∧ g x j = x)
-    (h : ∀ x y z w, f (g x y) (g z w) = g (f x z) (f y w)) : f = g := by
+def joinFromFish (m : Type u → Type u) (fish : {α β γ : Type u} → (α → m β) → (β → m γ) → α → m γ) :
+    m (m α) → m α :=
+  sorry
+
+def bindFromFish (m : Type u → Type u) (fish : {α β γ : Type u} → (α → m β) → (β → m γ) → α → m γ) :
+    m α → (α → m β) → m β :=
+  sorry
+
+/-
+## 2.9
+
+Implement these functions by looking carefully at the type signatures of what you're given.
+-/
+
+def yoneda (f : Type u → Type v) [Functor f] (g : {β : Type u} → (α → β) → f β) : f α :=
+  sorry
+
+def yoneda' (f : Type u → Type v) [Functor f] (y : f α) : {β : Type u} → (α → β) → f β :=
+  sorry
+
+/-
+## 2.10
+
+Implement the function below that returns `42` if the input string is `"lean"` and otherwise leaves it unchanged. Since the type of this function is pretty complicated, we'll implement it using tactics mode, which isn't just for proofs!
+-/
+
+def lean_to_42_type (x : String) : Type :=
+  if x = "lean" then ℕ else String
+
+def lean_to_42 (x : String) : lean_to_42_type x := by
   sorry

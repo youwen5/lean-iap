54622194682ba8441e3b85c4a6d1a2289e0a1932
Anthony Wang <a@unnamed.website>
2026-01-09 10:22:21 -0600

    Update office hours time

--- Advanced.lean
+++ Advanced.lean
@@ -1,4 +1,6 @@
+import Std.Tactic.Do
 import Mathlib
+
 -- https://leanprover.zulipchat.com/#narrow/channel/236446-Type-theory/topic/Paradoxes.20and.20Type.20Universes/with/538016579
 
 axiom Bad : Type
@@ -29,3 +31,29 @@ theorem false : False :=
 
 -- 'false' depends on axioms: [Bad, bad]
 #print axioms _root_.false
+
+
+
+
+
+
+
+
+
+-- TODO: prove binary search
+
+def Array.binarySearch (A : Array ℕ) (x : ℕ) := Id.run do
+  let n := A.size
+  let mut l : Fin (n + 1) := 0
+  let mut r : Fin (n + 1) := ⟨n, by grind⟩
+  while h : l < r do
+    let m : Fin (n + 1) := ⟨(l.val + r.val) / 2, by grind⟩
+    if A[m]'(by grind) < x then
+      l := m + 1
+    else
+      r := m
+  return l
+
+open Std.Do in
+lemma binarySearchCorrect (A : Array ℕ) (hA : A.Pairwise (· ≤ ·)) : ∀ i (hi : i < A.binarySearch x), A[i] < x := by
+  sorry
--- Basic.lean
+++ Basic.lean
@@ -1,1 +1,81 @@
 import Mathlib
+
+-- This is a comment
+
+/-
+This is also a comment
+
+/-
+Whoa, nested comments
+-/
+-/
+
+-- Variables are immutable
+def a := 1
+
+#eval a
+
+-- Get type of `a`
+#check a
+
+
+
+
+
+open symmDiff
+
+instance : Add (Set α) := ⟨(· ∆ ·)⟩
+
+@[simp]
+lemma add_def {a b : Set α} : a + b = a ∆ b := rfl
+
+instance : Zero (Set α) := ⟨{}⟩
+
+@[simp]
+lemma zero_def : (0 : Set α) = {} := rfl
+
+instance : Neg (Set α) := ⟨id⟩
+
+@[simp]
+lemma neg_def {a : Set α} : -a = a := rfl
+
+instance : Mul (Set α) := ⟨(· ∩ ·)⟩
+
+@[simp]
+lemma mul_def {a b : Set α} : a * b = a ∩ b := rfl
+
+instance : One (Set α) := ⟨.univ⟩
+
+@[simp]
+lemma one_def : (1 : Set α) = .univ := rfl
+
+example : CommRing (Set α) where
+  add_assoc := by
+    simp only [add_def]
+    grind
+  zero_add := by simp
+  add_zero := by simp
+  nsmul := nsmulRec
+  zsmul := zsmulRec
+  neg_add_cancel := by simp
+  add_comm := by
+    simp only [add_def]
+    grind
+  left_distrib a b c := by
+    ext x
+    simp [symmDiff_def]
+    grind
+  right_distrib a b c := by
+    ext x
+    simp [symmDiff_def]
+    grind
+  zero_mul := by simp
+  mul_zero := by simp
+  mul_assoc := by
+    simp only [mul_def]
+    grind
+  one_mul := by simp
+  mul_one := by simp
+  mul_comm := by
+    simp only [mul_def]
+    grind
